c526f1d8231b646148c7c7b20820195faffaaf4c
Nimira43 <n.rabone72@aol.com>
2024-12-19 20:42:24 +0000

    Corrected missing ] on line 8

--- src/App.jsx
+++ src/App.jsx
@@ -5,60 +5,61 @@ export default function App() {
   const [LowercaseLetters, setLowercaseLetters] = useState(false)
   const [UppercaseLetters, setUppercaseLetters] = useState(false)
   const [numbers, setNumbers] = useState(false)
-  const [symbols, setSymbols = useState(false)
+  const [symbols, setSymbols] = useState(false)
 
   return (
-    <div className='container'>
-      
-      <h1>Password Genie</h1>
-      <div className='password-generator'>
-        <p className='generated-password'>Generated Password</p>
-        <form>
-          <article>
-            <label htmlFor='password-length'>Password Length</label>
-            <input 
-              type='number' 
-              name='password-length' 
-              id='password-length' 
-              min={8} 
-              max={20}
-            />
-          </article>
-          <article>
-            <label htmlFor='lowercase-letters'>Lowercase Letters</label>
-            <input 
-              type='checkbox' 
-              name='lowercase-letters' 
-              id='lowercase-letters' 
-            />
-          </article>
-          <article>
-            <label htmlFor='uppercase-letters'>Uppercase Letters</label>
-            <input 
-              type='checkbox' 
-              name='uppercase-letters' 
-              id='uppercase-letters' 
-            />
-          </article>
-          <article>
-            <label htmlFor='numbers'>Numbers</label>
-            <input 
-              type='checkbox' 
-              name='numbers' 
-              id='numbers' 
-            />
-          </article>
-          <article>
-            <label htmlFor='symbols'>Symbols</label>
-            <input 
-              type='checkbox' 
-              name='symbols' 
-              id='symbols' 
-            />
-          </article>
-          <button type='Submit'>Generate Password</button>
-        </form>
+    <>
+      <div className='container'>
+          <h1>Password Genie</h1>
+        <div className='password-generator'>
+          <p className='generated-password'>Generated Password</p>
+          <form>
+            <article>
+              <label htmlFor='password-length'>Password Length</label>
+              <input 
+                type='number' 
+                name='password-length' 
+                id='password-length' 
+                min={8} 
+                max={20}
+              />
+            </article>
+            <article>
+              <label htmlFor='lowercase-letters'>Lowercase Letters</label>
+              <input 
+                type='checkbox' 
+                name='lowercase-letters' 
+                id='lowercase-letters' 
+              />
+            </article>
+            <article>
+              <label htmlFor='uppercase-letters'>Uppercase Letters</label>
+              <input 
+                type='checkbox' 
+                name='uppercase-letters' 
+                id='uppercase-letters' 
+              />
+            </article>
+            <article>
+              <label htmlFor='numbers'>Numbers</label>
+              <input 
+                type='checkbox' 
+                name='numbers' 
+                id='numbers' 
+              />
+            </article>
+            <article>
+              <label htmlFor='symbols'>Symbols</label>
+              <input 
+                type='checkbox' 
+                name='symbols' 
+                id='symbols' 
+              />
+            </article>
+            <button type='Submit'>Generate Password</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
