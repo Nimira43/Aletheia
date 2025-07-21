528db4ec514911f5ff88fe1564564a286779c611
Nimira43 <n.rabone72@aol.com>
2025-07-21 22:47:45 +0100

    Creating simple server.

--- HTMX/app.js
+++ HTMX/app.js
@@ -1,2 +1,12 @@
 import express from 'express'
 import { famousSayings } from './data/famousSaying.js'
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (re, res) => {
+  res.send('Hello HTMX World!')
+})
+
+app.listen(3000)
