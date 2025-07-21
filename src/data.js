b5788675ed64d4885bbb1ad898288a7993533903
Nimira43 <n.rabone72@aol.com>
2025-07-21 20:26:48 +0100

    New project for repo

--- src/data.js
+++
@@ -1,4 +0,0 @@
-export const nums = '0123456789'
-export const lowercase = 'abcdefghijklmnopqrstuvwxyz'
-export const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-export const syms = '~!@#$%^&*()_+}{|::?><}'
