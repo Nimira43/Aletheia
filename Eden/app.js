6737f2614c23500c9de2b6fbe4fc5b76915262e2
Nimira43 <n.rabone72@aol.com>
2025-11-15 16:59:08 +0000

    Brought in imports for app.js

---
+++ Eden/app.js
@@ -0,0 +1,4 @@
+import express from 'express'
+import { availableLocations } from './data/availableLocations.js'
+import renderLocationsPage from './views/index.js'
+import renderLocation from './views/components/location.js'
