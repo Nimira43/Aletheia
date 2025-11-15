df309977ee582797c5ff9e9e7db5a6155f4b7fb0
Nimira43 <n.rabone72@aol.com>
2025-11-15 05:19:41 +0000

    Working on availableLocations.js.

---
+++ Eden/data/availableLocations.js
@@ -0,0 +1,182 @@
+export const availableLocations = [
+  {
+    id: 'p1',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p2',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p3',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p4',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p5',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p6',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p7',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p8',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p9',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p10',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p11',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p12',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p13',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p14',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p15',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p16',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p17',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+  {
+    id: 'p18',
+    title: '',
+    image: {
+      src: '.jpg',
+      alt: '10 word description',
+    },
+    lat: 0,
+    lon: 0,
+  },
+]
